d49c6318dd41ace563b1334a76cdfad1a1b9c8c7
wp <wp@gmail.com>
2020-11-06 11:55:27 +0800

    提交了第一版的微信

---
+++ pages/adBook/adBook.js
@@ -0,0 +1,72 @@
+// pages/adBook/adBook.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        friend: [
+            { name: "新的朋友" },
+            { name: "仅聊天的朋友" },
+            { name: "群聊" }, { name: "标签" }, { name: "公众号" }, { name: "建驴" }, { name: "LeBron" }, { name: "马化疼" }, { name: "衰仔" }, { name: "小鹿" },
+        ],
+        letters: ['↑', "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+        bd: [1, 2, 3, 4, 88, 88, 88, 88, 88, 10]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,81 @@
+//index.js
+//获取应用实例
+const app = getApp()
+    // pages/find/find.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        msgSets: [
+            { title: "丰巢智能柜", msg: "丰巢邀请你加入S10院长群", date: "上午8:21" },
+            { title: "顺丰速运", msg: "快递费用明细通知", date: "上午7:33" },
+            { title: "订阅号消息", msg: "[19条] 🏀左右为篮:太疯狂了!老詹数项数据第一,字母哥", date: "昨天" },
+            { title: "微信支付", msg: "微信支付凭证", date: "昨天" },
+            { title: "文件传输助手", msg: "[图片]", date: "昨天" },
+            { title: "铠甲勇士", msg: "潘俊驴:[图片]", date: "昨天" },
+            { title: "上海名媛群", msg: "猛一:拼一个师大浴池双人浴", date: "昨天" },
+            { title: "涌优新鲜订", msg: "深秋已至,别忘了给自己加杯牛奶", date: "昨天" },
+            { title: "建驴", msg: "建驴领取了你的红包", date: "昨天" },
+            { title: "家得乐集团", msg: "家得乐集团要求使用你的地理位置", date: "昨天" }
+        ],
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
